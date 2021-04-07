f482b5802382ac22f66af4f969beef80b34f5e2b
rodrigo debian <correo.rodrigo@gmail.com>
2021-04-07 08:13:30 -0500

    added enum changes

---
+++ src/change_enum_value_in_place.rs
@@ -0,0 +1,50 @@
+
+use std::mem;
+use std::fmt;
+
+pub enum MyEnum {
+    A { name: String, x: u8 },
+    B { name: String }
+}
+
+impl core::fmt::Debug for MyEnum {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MyEnum::A { .. } => f.debug_struct("A").finish(),
+            MyEnum::B { .. } => f.debug_struct("B").finish()
+        }        
+    }
+}
+
+
+pub fn a_to_b(e: &mut MyEnum) {
+
+    // we mutably borrow `e` here. This precludes us from changing it directly
+    // as in `*e = ...`, because the borrow checker won't allow it. Therefore
+    // the assignment to `e` must be outside the `if let` clause.
+    *e = if let MyEnum::A { ref mut name, x: 0 } = *e {
+
+        // this takes out our `name` and put in an empty String instead
+        // (note that empty strings don't allocate).
+        // Then, construct the new enum variant (which will
+        // be assigned to `*e`, because it is the result of the `if let` expression).
+        MyEnum::B { name: mem::take(name) }
+
+    // In all other cases, we return immediately, thus skipping the assignment
+    } else { return }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn a_to_b_test() {
+        let name = "Rodrigo".to_string(); 
+        let mut a = MyEnum::A { name: name, x: 0 };
+
+        a_to_b(&mut a);
+
+        assert_eq!(format!("{:?}", a), "B");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod str_guidelines;
 pub mod default_ctr;
+pub mod change_enum_value_in_place;
