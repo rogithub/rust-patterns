eb7f8f161633858888b8f829f84ed0ae7da85119
rodrigo debian <correo.rodrigo@gmail.com>
2021-04-12 09:46:18 -0500

    builder comments

---
+++ src/creational/fold.rs
@@ -0,0 +1,8 @@
+// The data we will fold, a simple AST.
+
+/*
+The visitor pattern is closely related to fold. They share the concept of
+walking a data structure performing an operation on each node. However,
+the visitor does not create a new data structure nor consume the old one.
+
+*/
