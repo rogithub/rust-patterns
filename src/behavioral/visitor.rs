39280a63b4aff25bc86e48b7daa37d9998b89c0e
rodrigo debian <correo.rodrigo@gmail.com>
2021-04-12 07:42:22 -0500

    visitor progress

---
+++ src/behavioral/visitor.rs
@@ -0,0 +1,85 @@
+// The visitor pattern is useful anywhere that you want to apply an algorithm to 
+// heterogeneous data. If data is homogeneous, you can use an iterator-like pattern. 
+// Using a visitor object (rather than a functional approach) allows the visitor to 
+// be stateful and thus communicate information between nodes.
+
+// The data we will visit
+mod ast {
+    pub enum Stmt {
+        Expr(Expr),
+        Let(Name, Expr),
+    }
+
+    pub struct Name {
+        value: String,
+    }
+
+    pub enum Expr {
+        IntLit(i64),
+        Add(Box<Expr>, Box<Expr>),
+        Sub(Box<Expr>, Box<Expr>),
+    }
+}
+
+// The abstract visitor
+mod visit {
+    use ast::*;
+
+    pub trait Visitor<T> {
+        fn visit_name(&mut self, n: &Name) -> T;
+        fn visit_stmt(&mut self, s: &Stmt) -> T;
+        fn visit_expr(&mut self, e: &Expr) -> T;
+    }
+}
+
+use visit::*;
+use ast::*;
+
+// An example concrete implementation - walks the AST interpreting it as code.
+struct Interpreter;
+impl Visitor<i64> for Interpreter {
+    fn visit_name(&mut self, n: &Name) -> i64 { panic!() }
+    fn visit_stmt(&mut self, s: &Stmt) -> i64 {
+        match *s {
+            Stmt::Expr(ref e) => self.visit_expr(e),
+            Stmt::Let(..) => unimplemented!(),
+        }
+    }
+
+    fn visit_expr(&mut self, e: &Expr) -> i64 {
+        match *e {
+            Expr::IntLit(n) => n,
+            Expr::Add(ref lhs, ref rhs) => self.visit_expr(lhs) + self.visit_expr(rhs),
+            Expr::Sub(ref lhs, ref rhs) => self.visit_expr(lhs) - self.visit_expr(rhs),
+        }
+    }
+}
+
+pub fn walk_expr(visitor: &mut Visitor, e: &Expr) {
+    match *e {
+        Expr::IntLit(_) => {},
+        Expr::Add(ref lhs, ref rhs) => {
+            visitor.visit_expr(lhs);
+            visitor.visit_expr(rhs);
+        }
+        Expr::Sub(ref lhs, ref rhs) => {
+            visitor.visit_expr(lhs);
+            visitor.visit_expr(rhs);
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn visitor_testing() {
+        let visitor = Interpreter;
+        let e = Expr::IntLit(1);
+        walk_expr(mut visitor, e);
+    }
+}
+
